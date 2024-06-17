284c16b0e837b6e7702d9d5a4aa1d2db129efdb5
Efsun Peynirci <100719856+EfsunPeynirci@users.noreply.github.com>
2024-06-18 00:11:20 +0200

    Update RegisterViewModel.cs

--- WebAppHotelManagement/ViewModel/RegisterViewModel.cs
+++ WebAppHotelManagement/ViewModel/RegisterViewModel.cs
@@ -8,13 +8,14 @@ namespace WebAppHotelManagement.ViewModel
         [Display(Name = "User Name")]
         public string UserName { get; set; }
 
+        //EmailAddress gecerli eposta olmasını saglar
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [DataType(DataType.Password)]
+        [DataType(DataType.Password)]  //sifrenin yildizli olmasini saglar
         [Display(Name = "Password")]
         public string Password { get; set; }
 
